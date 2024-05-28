3eb8a32dd6c9f514ac0ce8ed5b601acc75f3eb0f
Pavelard0 <papitaspavel@gmail.com>
2024-05-28 15:34:02 -0600

    add click graficos

--- cargaconf.php
+++ cargaconf.php
@@ -4,7 +4,6 @@ return [
         // IMPORTANTE: Fecha siempre debe ir como segundo campo
         'siassa' => ['email', 'fecha', 'mensaje', 'nombre', 'telefono'],
         'prueba' => ['email', 'fecha', 'mensaje', 'nombre', 'telefono'],
-        'monkey' => ['email', 'fecha', 'mensaje', 'nombre', 'telefono', 'estado'],
-        'jair'   => ['nombre', 'fecha', 'numero', 'mensaje', 'email']
+        'monkey'   => ['nombre', 'fecha', 'telefono','carrera','area_de_interes','mensaje', 'email'],
     ],
 ];
--- chart.php
+++ chart.php
@@ -79,6 +79,7 @@ foreach ($tablas as $tabla) {
 $conn->close();
 ?>
 
+
 <div class="container">
     <div class="row text-center">
         <div class="col-md-12">
@@ -92,18 +93,30 @@ $conn->close();
             <form id="formMes" action="" method="get">
                 <select name="mes" id="mes" class="form-control my-2" style="font-size: 14px;">
                     <option disabled>Selecciona un mes</option>
-                    <option value="01" <?php if ($selectedMonth === '01') echo 'selected'; ?>>Enero</option>
-                    <option value="02" <?php if ($selectedMonth === '02') echo 'selected'; ?>>Febrero</option>
-                    <option value="03" <?php if ($selectedMonth === '03') echo 'selected'; ?>>Marzo</option>
-                    <option value="04" <?php if ($selectedMonth === '04') echo 'selected'; ?>>Abril</option>
-                    <option value="05" <?php if ($selectedMonth === '05') echo 'selected'; ?>>Mayo</option>
-                    <option value="06" <?php if ($selectedMonth === '06') echo 'selected'; ?>>Junio</option>
-                    <option value="07" <?php if ($selectedMonth === '07') echo 'selected'; ?>>Julio</option>
-                    <option value="08" <?php if ($selectedMonth === '08') echo 'selected'; ?>>Agosto</option>
-                    <option value="09" <?php if ($selectedMonth === '09') echo 'selected'; ?>>Septiembre</option>
-                    <option value="10" <?php if ($selectedMonth === '10') echo 'selected'; ?>>Octubre</option>
-                    <option value="11" <?php if ($selectedMonth === '11') echo 'selected'; ?>>Noviembre</option>
-                    <option value="12" <?php if ($selectedMonth === '12') echo 'selected'; ?>>Diciembre</option>
+                    <option value="01" <?php if ($selectedMonth === '01')
+                        echo 'selected'; ?>>Enero</option>
+                    <option value="02" <?php if ($selectedMonth === '02')
+                        echo 'selected'; ?>>Febrero</option>
+                    <option value="03" <?php if ($selectedMonth === '03')
+                        echo 'selected'; ?>>Marzo</option>
+                    <option value="04" <?php if ($selectedMonth === '04')
+                        echo 'selected'; ?>>Abril</option>
+                    <option value="05" <?php if ($selectedMonth === '05')
+                        echo 'selected'; ?>>Mayo</option>
+                    <option value="06" <?php if ($selectedMonth === '06')
+                        echo 'selected'; ?>>Junio</option>
+                    <option value="07" <?php if ($selectedMonth === '07')
+                        echo 'selected'; ?>>Julio</option>
+                    <option value="08" <?php if ($selectedMonth === '08')
+                        echo 'selected'; ?>>Agosto</option>
+                    <option value="09" <?php if ($selectedMonth === '09')
+                        echo 'selected'; ?>>Septiembre</option>
+                    <option value="10" <?php if ($selectedMonth === '10')
+                        echo 'selected'; ?>>Octubre</option>
+                    <option value="11" <?php if ($selectedMonth === '11')
+                        echo 'selected'; ?>>Noviembre</option>
+                    <option value="12" <?php if ($selectedMonth === '12')
+                        echo 'selected'; ?>>Diciembre</option>
                 </select>
             </form>
         </div>
@@ -112,7 +125,7 @@ $conn->close();
                 <select name="anio" id="anio" class="form-control my-2" style="font-size: 14px;">
                     <option disabled>Selecciona un año</option>
                     <?php
-                    for ($i = 2020; $i <= date('Y'); $i++) { // Puedes ajustar el rango de años según tus necesidades
+                    for ($i = 2020; $i <= date('Y'); $i++) {
                         $anioSeleccionadoHTML = ($selectedYear == $i) ? 'selected' : '';
                         printf('<option value="%s" %s>%s</option>', $i, $anioSeleccionadoHTML, $i);
                     }
@@ -134,7 +147,7 @@ $conn->close();
     function submitForms() {
         const formMes = document.getElementById('formMes');
         const formAnio = document.getElementById('formAnio');
-        
+
         const mes = formMes.mes.value;
         const anio = formAnio.anio.value;
 
@@ -147,62 +160,42 @@ $conn->close();
     }
 
     const ctx = document.getElementById('myChart');
+    const labels = <?php echo json_encode($labels); ?>;
+    const leads = [<?php echo implode(',', $leads); ?>];
 
-    const backgroundColor = Array.from({
-        length: <?php echo count($labels); ?>
-    }, () => 'rgba(239, 72, 46, 0.8)');
-
-    new Chart(ctx, {
+    const myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: <?php echo json_encode($labels); ?>,
+            labels: labels,
             datasets: [{
                 label: '# Leads',
-                data: [<?php echo implode(',', $leads); ?>],
-                backgroundColor: backgroundColor, // Asignar el color único a todas las barras
+                data: leads,
+                backgroundColor: 'rgba(239, 72, 46, 0.8)',
                 borderWidth: 1
             }]
         },
         options: {
             scales: {
                 y: {
-                    beginAtZero: true,
-                    ticks: {
-                        font: {
-                            family: 'Kumbh Sans', // Cambiar la fuente a 'Kumbh Sans'
-                        },
-                        precision: 0 // Mostrar valores enteros
-                    }
-                },
-                x: {
-                    ticks: {
-                        font: {
-                            family: 'Kumbh Sans', // Cambiar la fuente a 'Kumbh Sans'
-                        }
-                    }
+                    beginAtZero: true
                 }
             },
-            plugins: {
-                legend: {
-                    labels: {
-                        font: {
-                            family: 'Kumbh Sans', // Cambiar la fuente a 'Kumbh Sans'
-                        }
-                    }
-                },
-                tooltip: {
-                    titleFont: {
-                        family: 'Kumbh Sans', // Cambiar la fuente a 'Kumbh Sans'
-                        weight: 'lighter'
-                    },
-                    bodyFont: {
-                        family: 'Kumbh Sans', // Cambiar la fuente a 'Kumbh Sans'
-                    }
+            onClick: (event, elements) => {
+                if (elements.length > 0) {
+                    const chartElement = elements[0];
+                    const label = myChart.data.labels[chartElement.index].toLowerCase(); // Convertir a minúsculas
+                    const url = `http://localhost/crm-2024/data.php?id=${label}&mes=<?php echo $selectedMonth; ?>&anio=<?php echo $selectedYear; ?>`;
+                    window.location.href = url;
                 }
             }
         }
     });
 </script>
+
+<?php
+include 'layouts/footer.php';
+?>
+
 
 <?php
 include 'layouts/footer.php';
